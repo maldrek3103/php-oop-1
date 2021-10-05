328dee582bf77643f307f1c494310fd6e8b03285
maldrek3103 <cpisanu96@gmail.com>
2021-10-05 13:52:11 +0200

    added construct function

--- index.php
+++ index.php
@@ -5,4 +5,11 @@ class Movie
     public $name;
     public $genre;
     public $year;
+
+    function __construct($_name, $_genre, $_year)
+    {
+        $this->name = $_name;
+        $this->genre = $_genre;
+        $this->year = $_year;
+    }
 }
